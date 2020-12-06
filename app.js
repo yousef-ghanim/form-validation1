a0790bc22a643dd25c61a2d145ab720e0bc9cc41
yousef-ghanim <yousefghanim85@gmail.com>
2020-12-06 11:52:31 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const name = document.getElementById("name");
+const password = document.getElementById("password");
+const form = document.getElementById("form");
+const error = document.getElementById("error");
+
+
+form.addEventListener("submit", (e)=>{
+    let messeges = [];
+   
+    if (name.value == "" || name.value == null){
+messeges.push("please ensert name");
+    }
+
+   if(password.value.length <= 6){
+       e.preventDefault();
+       messeges.push("password must be longer than 6 charecters");
+   } else if(password.value.length >= 20){
+       e.preventDefault();
+       messeges.push("password must be shorter than 20 charecters");
+   } else if(password.value === "password"){
+       e.preventDefault();
+       messeges.push("password cannot be 'password'");
+   } 
+    
+if (messeges.length > 0){
+ e.preventDefault();
+ error.innerHTML = messeges.join(", ");
+}
+})  
