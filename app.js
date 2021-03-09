94da68b53e2fd7b826620e8ca7abce7ba0dd8ecd
yousef-ghanim <yousefghanim85@gmail.com>
2021-03-09 17:28:14 +0000

    fixing format

--- app.js
+++ app.js
@@ -3,27 +3,26 @@ const password = document.getElementById("password");
 const form = document.getElementById("form");
 const error = document.getElementById("error");
 
+form.addEventListener("submit", (e) => {
+  let messeges = [];
 
-form.addEventListener("submit", (e)=>{
-    let messeges = [];
-   
-    if (name.value == "" || name.value == null){
-messeges.push("please ensert name");
-    }
+  if (name.value == "" || name.value == null) {
+    messeges.push("please ensert name");
+  }
 
-   if(password.value.length <= 6){
-       e.preventDefault();
-       messeges.push("password must be longer than 6 charecters");
-   } else if(password.value.length >= 20){
-       e.preventDefault();
-       messeges.push("password must be shorter than 20 charecters");
-   } else if(password.value === "password"){
-       e.preventDefault();
-       messeges.push("password cannot be 'password'");
-   } 
-    
-if (messeges.length > 0){
- e.preventDefault();
- error.innerHTML = messeges.join(", ");
-}
-})  
+  if (password.value.length <= 6) {
+    e.preventDefault();
+    messeges.push("password must be longer than 6 charecters");
+  } else if (password.value.length >= 20) {
+    e.preventDefault();
+    messeges.push("password must be shorter than 20 charecters");
+  } else if (password.value === "password") {
+    e.preventDefault();
+    messeges.push("password cannot be 'password'");
+  }
+
+  if (messeges.length > 0) {
+    e.preventDefault();
+    error.innerHTML = messeges.join(", ");
+  }
+});
